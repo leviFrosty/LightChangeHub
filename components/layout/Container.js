0fa6a71bb9859d1c911cc09a93d23fc89bd13a69
Levi Wilkerson <chelleas123@gmail.com>
2022-01-31 14:34:35 -0500

    fix: normalized container component to be more extendable

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -1,9 +1,6 @@
 export default function Container({ className, children, ...props }) {
   return (
-    <div
-      className={`mx-auto flex flex-col px-2 md:px-8 ${className}`}
-      {...props}
-    >
+    <div className={`mx-auto flex px-2 md:px-8 ${className}`} {...props}>
       {children}
     </div>
   );
