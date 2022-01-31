1023ed0b8e74cac5dc557210c89c70428c995596
Levi Wilkerson <chelleas123@gmail.com>
2022-01-31 12:43:10 -0500

    feat: add global _app component for authcontext and theme contexts

---
+++ components/theme/ToggleTheme.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import MoonHallow from "../../public/icons/moon-regular.svg";
+import MoonSolid from "../../public/icons/moon-solid.svg";
+
+export default function ToggleTheme({ className, ...props }) {
+  const [theme, settheme] = useState("light");
+
+  // Toggles theme key in localstorage
+  const handleClick = () => {
+    if (localStorage.theme === "dark") {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  };
+
+  // Sets loaded state to "theme" from localstorage
+  useEffect(() => {
+    settheme(localStorage.getItem("theme"));
+  }, []);
+
+  return (
+    <div onClick={handleClick} className="cursor-pointer">
+      {theme === "dark" ? (
+        <MoonSolid className={`text-text-light h-5 w-5 ${className}`} />
+      ) : (
+        <MoonHallow className={`text-text-light h-5 w-5 ${className}`} />
+      )}
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserProvider } from "../contexts/userContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../lib/fbInstance";
+import handleTheme from "../lib/handleTheme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState({ user: null, isLoading: true });
+
+  useEffect(() => {
+    // Handles tailwindcss theme
+    console.log(localStorage.getItem("theme"));
+    if (localStorage.getItem("theme") === null) {
+      localStorage.setItem("theme", "dark");
+    }
+
+    handleTheme();
+    // Listens for auth state changes then sets global context
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({ user, isLoading: false });
+      } else {
+        setUser({ user: null, isLoading: false });
+      }
+    });
+  }, []);
+
+  return (
+    <UserProvider value={user}>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
